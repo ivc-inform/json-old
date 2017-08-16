d5a00835589cbc97b91dbee97133f1a4412eb2a9
andrew <uandrew1965@gmail.com>
2017-08-16 12:18:41 +0300

    .

---
+++ build.sbt
@@ -0,0 +1,43 @@
+sbtPlugin := true
+
+name := "json"
+
+//enablePlugins(GitVersioning)
+
+organization := "com.simplesys"
+
+version := "1.0.1"
+
+scalaVersion := "2.10.6"
+
+scalacOptions := Seq(
+    "-feature",
+    "-language:postfixOps",
+    "-language:higherKinds",
+    "-language:implicitConversions",
+    "-deprecation",
+    "-unchecked"
+)
+
+description := ""
+
+libraryDependencies ++= {
+	//val ssysCoreVersion = "1.2.100"
+	val ssysCoreVersion = "1.3-SNAPSHOT"
+	Seq(       
+    		"com.simplesys.core" %% "scala-gen" % ssysCoreVersion
+	)
+}	
+
+publishArtifact in(Compile, packageBin) := true
+
+publishArtifact in(Test, packageBin) := false
+
+publishArtifact in(Compile, packageDoc) := false
+
+publishArtifact in(Compile, packageSrc) := true
+
+publishMavenStyle := true
+
+//git.baseVersion := "1.3"
+
---
+++ src/main/scala/com/simplesys/plugins/json/JSONPlugin.scala
@@ -0,0 +1,30 @@
+package com.simplesys.plugins.json
+
+import sbt.Keys._
+import sbt._
+import sbt.plugins.JvmPlugin
+import com.simplesys.json.GeneratorThisFunctions
+
+object JSONPlugin extends AutoPlugin {
+
+    override def requires = JvmPlugin
+
+    val maxArity = SettingKey[Int]("maxArity", "How max arity This Functions")
+    val outputDir = SettingKey[File]("output-directory", "Directory where the scala files should be placed")
+    val generateThisFunctions = TaskKey[Seq[File]]("generate-this function", "Generate scala sources")
+
+    object autoImport {
+        val maxArity = JSONPlugin.maxArity
+    }
+
+    override val projectSettings: Seq[Setting[_]] = inConfig(Compile)(Seq(
+        outputDir := (sourceManaged in Compile) / "com" / "simplesys" / "json",
+        generateThisFunctions := genThisFunction(outputDir.value, maxArity.valie).value,
+        sourceGenerators += generateThisFunctions
+    ))
+
+
+    def genThisFunction(outputDir: File, maxArity: Int): Seq[File] = (
+       GeneratorThisFunctions.genThisFunctions(outputDir, maxArity)
+    )
+}
